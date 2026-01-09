4effbe3bd53b2656c7b0da4c1a679f41b0c9cc5c
adee0210 <anhduc02102004@gmail.com>
2026-01-09 20:17:30 +0700

    update

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,25 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cqv", function()
+  local root = vim.fn.getcwd()
+
+  local cmd = table.concat({
+    "cd " .. root,
+    "python3 -m venv .venv || true",
+    ".venv/bin/python -m pip install --upgrade pip",
+    "[ -f requirements.txt ] && .venv/bin/python -m pip install -r requirements.txt || echo 'No requirements.txt'",
+  }, " && ")
+
+  vim.cmd("terminal " .. cmd)
+end, { desc = "Create .venv + install requirements (show output)" })
+
+vim.keymap.set("n", "<leader>rp", function()
+  if vim.bo.filetype ~= "python" then
+    print("Chỉ dùng cho file Python!")
+    return
+  end
+  vim.cmd("w")
+  vim.cmd("belowright term python %")
+end, { desc = "Run Python file in terminal" })
