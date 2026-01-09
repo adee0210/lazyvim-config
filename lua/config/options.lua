09ac5eeab42a4ebd48370542a48edcf35b091e16
adee0210 <anhduc02102004@gmail.com>
2026-01-10 02:14:01 +0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,8 +5,6 @@
 vim.opt.number = true -- hiển thị số dòng
 vim.opt.relativenumber = false -- không hiển thị số dòng tương đối
 vim.opt.wrap = false -- hiển thị dòng văn bản như một dòng duy nhất
-
 vim.opt.backup = false -- vô hiệu hóa việc tạo file sao lưu
 vim.opt.writebackup = false -- ngăn chặn việc chỉnh sửa các file đang được chỉnh sửa ở nơi khác
 vim.opt.swapfile = false -- Tắt tính năng tạo file swap
-vim.opt.wrap = false -- hiển thị dòng văn bản như một dòng duy nhất
